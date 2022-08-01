fbd0fea3f29c6d1381e1b8ee98d5be57efdf0bb9
Zeynel Şahin <zynl.shn@hotmail.com>
2022-08-01 10:39:44 +0300

    AJAX example

---
+++ AJAX/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app =  builder.Build();
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
